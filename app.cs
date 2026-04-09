21b152941e3bef88574a273764184607fb204b76
rey-mskvn <rey-mskvn@yandex.ru>
2026-04-09 19:16:58 +0500

    Второй коммит: создана ветка subtrac

--- app.cs
+++ app.cs
@@ -1,6 +1,9 @@
 ﻿    using System;
    class App {
-       static void Main() {
+       static void Main() 
+       {
            Console.WriteLine("Калькулятор");
-       }
+        }
+        Subtract(a, b)
+       { return a - b; }
    }
